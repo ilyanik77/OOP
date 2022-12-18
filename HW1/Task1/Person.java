67b53a3145a2a9c20bf67a9dade006d0b4107594
ilyanik77 <ilyakhlamov77@gmail.com>
2022-12-18 22:39:27 +0300

    Добавил Person.java

---
+++ HW1/Task1/Person.java
@@ -0,0 +1,13 @@
+package HW1.Task1;
+
+public class Person {
+    public String fullName;
+
+    public Person(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
