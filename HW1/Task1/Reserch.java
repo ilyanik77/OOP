45313a1d1c38be9be6033e7554f7467fe79dc7ad
ilyanik77 <ilyakhlamov77@gmail.com>
2022-12-18 22:41:51 +0300

    Добавил Reserch.java

---
+++ HW1/Task1/Reserch.java
@@ -0,0 +1,21 @@
+package HW1.Task1;
+
+import java.util.ArrayList;
+
+public class Reserch {
+    ArrayList<String> result = new ArrayList<>();
+    ArrayList<Node> tree;
+
+    public Reserch(GeoTree geoTree) {
+        tree = geoTree.getTree();
+    }
+
+    public ArrayList<String> spend(Person p, Relationship re) {
+        for (Node t : tree) {
+            if (t.p1.fullName == p.fullName && t.re == re) {
+                result.add(t.p2.fullName);
+            }
+        }
+        return result;
+    }
+}
