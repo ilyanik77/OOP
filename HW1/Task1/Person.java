01c999457c6aec52cbccee50c34e57d006ca3c7b
ilyanik77 <ilyakhlamov77@gmail.com>
2022-12-24 21:31:47 +0300

    Внес изменения в файл Person.java

--- HW1/Task1/Person.java
+++ HW1/Task1/Person.java
@@ -2,12 +2,25 @@ package HW1.Task1;
 
 public class Person {
     public String fullName;
+    public int age;
 
-    public Person(String fullName) {
+    public Person(String fullName, int age) {
         this.fullName = fullName;
+        this.age = age;
     }
 
     public String getFullName() {
         return fullName;
+    }
+
+    public void setFullName (String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge () {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
     }
 }
