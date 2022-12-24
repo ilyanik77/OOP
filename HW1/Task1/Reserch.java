291904eff5fe626b3135fe1f666d9bc770a02a44
ilyanik77 <ilyakhlamov77@gmail.com>
2022-12-24 21:51:17 +0300

    Внес изменения в файл Reserch.java

--- HW1/Task1/Reserch.java
+++ HW1/Task1/Reserch.java
@@ -1,9 +1,11 @@
 package HW1.Task1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Reserch {
     ArrayList<String> result = new ArrayList<>();
+    ArrayList<String> resultAgeSearch = new ArrayList<>();
     ArrayList<Node> tree;
 
     public Reserch(GeoTree geoTree) {
@@ -17,5 +19,20 @@ public class Reserch {
             }
         }
         return result;
+    }
+
+    public ArrayList<String> ageSearch() {
+        System.out.print("Ограничение по возрасту: ");
+        Scanner in = new Scanner(System.in);
+        int age = in.nextInt();
+        System.out.println("Список людей,которые младше " + age + ":");
+
+        for (Node t : tree) {
+
+            if (t.p1.age <= age && !resultAgeSearch.contains(t.p1.fullName)) {
+                resultAgeSearch.add(t.p1.fullName);
+            }
+        }
+        return resultAgeSearch;
     }
 }
