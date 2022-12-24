9d66370f4153264fbca1be374670892226ff5719
ilyanik77 <ilyakhlamov77@gmail.com>
2022-12-24 21:28:39 +0300

    Внес изменения в файл Main.java

--- HW1/Task1/Main.java
+++ HW1/Task1/Main.java
@@ -2,11 +2,11 @@ package HW1.Task1;
 
 public class Main {
     public static void main(String[] args) {
-        Person irina = new Person("Ирина");
-        Person vasya = new Person("Вася");
-        Person masha = new Person("Маша");
-        Person jane = new Person("Женя");
-        Person ivan = new Person("Ваня");
+        Person irina = new Person("Ирина", 51);
+        Person vasya = new Person("Вася", 25);
+        Person masha = new Person("Маша", 29);
+        Person jane = new Person("Женя", 7);
+        Person ivan = new Person("Ваня", 5);
         GeoTree gt = new GeoTree();
         gt.append(irina, vasya);
         gt.append(irina, masha);
@@ -14,6 +14,9 @@ public class Main {
         gt.append(vasya, ivan);
 
         System.out.println(new Reserch(gt).spend(irina,
+                Relationship.parent));
+
+        System.out.println(new Reserch(gt).spend(vasya,
                 Relationship.parent));
     }
 }
