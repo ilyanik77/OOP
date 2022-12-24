02e97ed7624fceeace75ee0e2cbf262ca6f3ab63
ilyanik77 <ilyakhlamov77@gmail.com>
2022-12-24 23:40:00 +0300

    Добавил Wardrobe.java

---
+++ HW1/Task2/Wardrobe.java
@@ -0,0 +1,17 @@
+package HW1.Task2;
+
+public class Wardrobe {
+    private String status;
+
+    public Wardrobe(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
