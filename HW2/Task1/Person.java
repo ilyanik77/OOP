f70982610c310f0692d9480440bb008e69168b2f
ilyanik77 <ilyakhlamov77@gmail.com>
2023-01-05 18:08:12 +0300

    Изменил Person.java

--- HW2/Task1/Person.java
+++ HW2/Task1/Person.java
@@ -3,6 +3,7 @@ package HW2.Task1;
 public class Person {
     public String name;
     public int age;
+    private String status;
 
     public int getAge() {
         return age;
@@ -18,5 +19,37 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void feedCat (String status, Cat cat) {
+        if (status == "owener") {
+
+            cat.catComeIn(cat.getName());
+            cat.animalTalk();
+            cat.eat();
+        } else {
+
+            cat.move();
+        }
+    }
+
+    public void feedDog (String status, Dog dog) {
+        if (status == "owener") {
+
+            dog.dogComeIn(dog.getName());
+            dog.animalTalk();
+            dog.eat();
+        } else {
+
+            dog.move();
+        }
     }
 }
