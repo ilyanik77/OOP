fe62e6d2682041f477ec605d083a46fe69f0d5e7
ilyanik77 <ilyakhlamov77@gmail.com>
2023-01-05 17:17:39 +0300

    Добавил интерфейс к Dog.java

--- HW2/Task1/Dog.java
+++ HW2/Task1/Dog.java
@@ -1,9 +1,27 @@
 package HW2.Task1;
 
-public class Dog extends Animal {
+public class Dog extends Animal implements Eat, Move, AnimalTalk {
 
     public Dog(String name) {
         super(name);
+    }
+
+    @Override
+    public void animalTalk() {
+        System.out.println("Собака " + name + " лает: Гав-Гав ");
+
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Собака " + name + " гуляет ");
+
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Собака " + name + " поела ");
+
     }
 
 }
