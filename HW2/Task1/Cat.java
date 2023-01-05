21d4057221b16e96f3cab84f7704fbed78937613
ilyanik77 <ilyakhlamov77@gmail.com>
2023-01-05 17:12:47 +0300

    Добавил интерфейс к Cat.java

--- HW2/Task1/Cat.java
+++ HW2/Task1/Cat.java
@@ -1,9 +1,27 @@
 package HW2.Task1;
 
-public class Cat extends Animal {
+public class Cat extends Animal implements Eat, Move, AnimalTalk {
 
     public Cat(String name) {
         super(name);
+    }
+
+    @Override
+    public void animalTalk() {
+        System.out.println("Кошка " + name + " мяукает: Мяу ");
+
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Кошка " + name + " гуляет ");
+
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Кошка " + name + " поела ");
+
     }
 
 }
